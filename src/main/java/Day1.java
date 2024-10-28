d2b8a54806797af6ea4ade299376ef786017b871
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-10-28 23:31:21 +0100

    Day 1 File Reader

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,3 +1,31 @@
-public class Day1 {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hehe");
+        ArrayList<Integer> input = readInput();
+        System.out.println(input);
+    }
+
+    public static ArrayList<Integer> readInput() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        try {
+            File input = new File("./src/main/resources/Input1.txt");
+            Scanner reader = new Scanner(input);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                if (!line.isEmpty()) {
+                    result.add(Integer.parseInt(line));
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        return result;
+    }
 }
