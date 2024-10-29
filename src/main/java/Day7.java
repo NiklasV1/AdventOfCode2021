a1ef377a939135866aafe6166e994792a85ef1de
NiklasV1 <niklas.vaethbrueckner@gmail.com>
2024-10-29 21:05:31 +0100

    Day 7 Part 2

--- src/main/java/Day7.java
+++ src/main/java/Day7.java
@@ -15,8 +15,8 @@ public class Day7 {
         System.out.printf("Result 1: %d%n", result1);
 
         // Part 2
-//        long result2 = evaluate2(input, 256);
-//        System.out.printf("Result 2: %d%n", result2);
+        int result2 = evaluate2(input);
+        System.out.printf("Result 2: %d%n", result2);
     }
 
     public static List<Integer> readInput() {
@@ -45,16 +45,41 @@ public class Day7 {
     public static int evaluate1(List<Integer> input) {
         Collections.sort(input);
         if (input.size() % 2 == 0) {
-            return Math.min(calculateFuelCost(input,input.get(input.size()/2)), calculateFuelCost(input,input.get(input.size()/2)));
+            return Math.min(calculateFuelCost1(input, input.get(input.size() / 2)), calculateFuelCost1(input, input.get(input.size() / 2)));
         } else {
-            return calculateFuelCost(input,input.get(input.size()/2));
+            return calculateFuelCost1(input, input.get(input.size() / 2));
         }
     }
 
-    public static int calculateFuelCost(List<Integer> input, int median) {
+    public static int calculateFuelCost1(List<Integer> input, int median) {
         int cost = 0;
         for (Integer value : input) {
             cost += Math.abs(value - median);
+        }
+        return cost;
+    }
+
+    public static int evaluate2(List<Integer> input) {
+        int sum = 0;
+        for (Integer value : input) {
+            sum += value;
+        }
+        int averageLow = (int) Math.floor((double) sum / input.size());
+        int averageHigh = (int) Math.ceil((double) sum / input.size());
+
+        System.out.println(averageLow);
+        System.out.println(averageHigh);
+
+        return Math.min(calculateFuelCost2(input, averageLow), calculateFuelCost2(input, averageHigh));
+    }
+
+    public static int calculateFuelCost2(List<Integer> input, int average) {
+        int cost = 0;
+        for (Integer value : input) {
+            int steps = Math.abs(value - average);
+            for (int i = 1; i <= steps; i++) {
+                cost += i;
+            }
         }
         return cost;
     }
